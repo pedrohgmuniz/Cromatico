d0baf2e7a6f97ff03705c0e92c5df3453e4c3efb
Pedro Muniz <phgmuniz@gmail.com>
2023-05-12 13:54:08 -0300

    Second commit, just getting everything ready

---
+++ Cromatico/CromaticoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CromaticoApp.swift
+//  Cromatico
+//
+//  Created by Pedro Muniz on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CromaticoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
